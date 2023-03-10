f39c1994cde96a564aa276c9ffa95b1e705c3c53
wickedbaba <aryamansinghranaasr@gmail.com>
2023-03-10 11:40:57 +0530

    finished working with rune  - basic

---
+++ rune.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+var pl = fmt.Println
+
+func main() {
+
+	rStr := "abcedgfh"
+
+	pl("Rune Count :", utf8.RuneCountInString(rStr))
+
+	for i, runeVal := range rStr {
+		fmt.Printf("%d : %#U : %c\n", i, runeVal, runeVal) // in Printf, you always end with a "\n" character
+	}
+}
